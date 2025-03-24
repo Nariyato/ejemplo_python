e7d4609ca860a64bc6c6c59216d9c918edf5454f
Nariyato <sir.alan.bugueno@gmail.com>
2025-03-24 12:43:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ try:
     suma = int(num1) + int(num2)
     #int -> transformar a numero entero
     print(f"la suma es: {suma} y {suma}, ademas es {suma}")
+    #la "f" es para mostrar variables entre {}, en vez de usar la coma (,). es más práctico
 except BaseException as error:
     #si hay un error, pasa lo de abajo, en vez de "caerse"
     print("ingreso erroneo de datos")
