2738db2f4482765a8869dbcead3f09962377e0de
Nariyato <sir.alan.bugueno@gmail.com>
2025-03-24 12:35:31 -0300

    mi primer archivo

---
+++ main.py
@@ -0,0 +1,13 @@
+print("sumar 2 numeros")
+num1 = input("Ingrese numero 1: ")
+num2 = input("Ingrese numero 2: ")
+try:
+    suma = int(num1) + int(num2)
+    #int -> transformar a numero entero
+    print(f"la suma es: {suma} y {suma}, ademas es {suma}")
+except BaseException as error:
+    #si hay un error, pasa lo de abajo, en vez de "caerse"
+    print("ingreso erroneo de datos")
+    
+print("termino")
+# ctr + s = grabar/guardar
